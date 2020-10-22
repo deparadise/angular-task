e50901440124fa79aca562fcb49db720aa2754f9
David Pineda <david.pineda@deparadise.com>
2020-10-21 21:36:21 -0700

    Add UserProfileListComponent as a route level component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,33 @@
-import { Routes } from '@angular/router';
-import { PageNotFoundComponent } from '@core/layout/page-not-found';
-import { HomePageComponent } from '@features/home-page';
-import { ProfileDetailComponent } from '@features/profile/profile-detail';
+import { Routes } from "@angular/router";
+import { PageNotFoundComponent } from "@core/layout/page-not-found";
+import { HomePageComponent } from "@features/home-page";
+import { UserProfileListComponent } from "@features/user-profile-list/user-profile-list.component";
+import { ProfileDetailComponent } from "@features/profile/profile-detail";
 
 export const appRoutes: Routes = [
     {
+        component: UserProfileListComponent,
+        data: { name: "profileList" },
+        path: "user/profiles",
+    },
+    {
         component: ProfileDetailComponent,
-        data: { name: 'profileDetail' },
-        path: 'profile'
+        data: { name: "profileDetail" }, // todo << where is this tield to?
+        path: "profile",
     },
     {
         component: PageNotFoundComponent,
-        data: { name: 'pageNotFound' },
-        path: '404'
+        data: { name: "pageNotFound" },
+        path: "404",
     },
     {
         component: HomePageComponent,
-        data: { name: 'homePage' },
-        path: ''
+        data: { name: "homePage" },
+        path: "",
     },
     {
-        data: { name: 'pageNotFound' },
-        path: '**',
-        redirectTo: '/404'
-    }
+        data: { name: "pageNotFound" },
+        path: "**",
+        redirectTo: "/404",
+    },
 ];
