d5fcdcacb39b1cb9c6176da290c77217e0267f2c
David Pineda <david.pineda@deparadise.com>
2020-10-21 21:28:19 -0700

    ng generate component features/user-profile-details

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,11 +3,13 @@ import { LayoutModule } from '@core/layout/layout.module';
 import { HomePageComponent } from './home-page';
 import { ProfileModule } from './profile/profile.module';
 import { UserProfileListComponent } from './user-profile-list/user-profile-list.component';
+import { UserProfileDetailsComponent } from './user-profile-details/user-profile-details.component';
 
 @NgModule({
     declarations: [
         HomePageComponent,
-        UserProfileListComponent
+        UserProfileListComponent,
+        UserProfileDetailsComponent
     ],
     entryComponents: [
         HomePageComponent
